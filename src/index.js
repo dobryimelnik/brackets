2b70acc4fa1dd69026f097df56b53afa18f42a2b
Oleg Marjankov <marjan.om84@gmail.com>
2023-02-23 23:30:50 +0300

    feat: created stack algoritmm

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let pairs = {};
+  const openBrackets = bracketsConfig.map(item => item[0]);
+  
+  bracketsConfig.forEach((item) => {
+    pairs[item[1]] = item[0]; 
+  });
+
+  let stack = [];
+  
+  for(let x = 0; x < str.length; x++) {
+    let currentSymbol = str[x];
+
+    if(openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if(stack.length == 0) {
+        return false;
+      }
+      let topElem = stack[stack.length - 1];
+      if(pairs[currentSymbol] == topElem) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return (stack == 0);
 }
+
