d2d0144706dd610fe42157933e37b9c20060e6d1
Oleg Marjankov <marjan.om84@gmail.com>
2023-02-27 15:39:20 +0300

    feat: deleted pairs off twins

--- src/index.js
+++ src/index.js
@@ -1,26 +1,47 @@
 module.exports = function check(str, bracketsConfig) {
-  
-  let pairs = {};
-  const openBrackets = bracketsConfig.map(item => item[0]);
-  
-  bracketsConfig.forEach((item) => {
-    pairs[item[1]] = item[0]; 
-  });
+  let map = new Map(bracketsConfig);
+  // console.log(map);
+  let values = [];
+  let keys = [];
+  let twins = [];
 
+  for(let value of map.values()) {
+    keys.push(value);
+  }
+  for(let key of map.keys()) {
+    values.push(key);
+  }
+  console.log(keys)
+  let pairs = {};
+
+  for(let i = 0; i < values.length; i++) {
+    if(values[i] == keys[i]) {
+      twins.push(values[i])
+    }
+    pairs[keys[i]] = values[i];
+  }
+
+  twins.forEach(item => {
+    while(str.includes(`${item}${item}`)) {    
+      str = str.replace(`${item}${item}`, '')
+    }
+  })
+  console.log(str)
   let stack = [];
   
   for(let x = 0; x < str.length; x++) {
     let currentSymbol = str[x];
 
-    if(openBrackets.includes(currentSymbol)) {
+    if(values.includes(currentSymbol)) {
       stack.push(currentSymbol);
-    } else {
-      if(stack.length == 0) {
+    } else  {
+      if(stack.length != 0) {        
+        let topElem = stack[stack.length - 1];
+        if(pairs[currentSymbol] == topElem) {
+          stack.pop();
+        } else {
         return false;
-      }
-      let topElem = stack[stack.length - 1];
-      if(pairs[currentSymbol] == topElem) {
-        stack.pop();
+        }
       } else {
         return false;
       }
